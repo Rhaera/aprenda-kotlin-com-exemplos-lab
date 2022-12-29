a5473116e23dff52c5ee98e42da8bc8a7de6c308
Rhaera <89935661+Rhaera@users.noreply.github.com>
2022-12-29 13:30:17 -0300

    Change 01

--- desafio.kt
+++ desafio.kt
@@ -4,7 +4,7 @@ enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
